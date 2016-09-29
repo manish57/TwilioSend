87d5ca97c0c32d80775a697b37c00bcf383bec29
manish57 <manish3257@gmail.com>
2016-09-29 19:53:50 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+ 
+// ==== Control Vars =======
+$strFromNumber = "+19177463100";
+$strToNumber = "+15512636012";
+$strMsg = "Hello From Manish Chaudhari"; 
+$aryResponse = array();
+ 
+
+    // include the Twilio PHP library - download from 
+    // http://www.twilio.com/docs/libraries/
+    require_once ("inc/Services/Twilio.php");
+ 
+    // set our AccountSid and AuthToken - from www.twilio.com/user/account
+    $AccountSid = "ACb975b7c314631a7092e26c8c4e75b9c1";
+    $AuthToken = "5655cffc023d4e02c0814b1688ff0e36";
+ 
+    // ini a new Twilio Rest Client
+    $objConnection = new Services_Twilio($AccountSid, $AuthToken);
+
+
+    // Send a new outgoinging SMS by POSTing to the SMS resource */
+    $bSuccess = $objConnection->account->sms_messages->create(
+        
+        $strFromNumber, 	// number we are sending From 
+        $strToNumber,           // number we are sending To
+        $strMsg			// the sms body
+    );
+
+		
+    $aryResponse["SentMsg"] = $strMsg;
+    $aryResponse["Success"] = true;
+    
+    
+    echo json_encode($aryResponse);
